2a6034d7aecd0e67aa7da6166fd7e62119295b73
Jason Turner <jason@emptycrate.com>
2015-09-06 20:29:54 -0600

    Add unreachable code test for enum

---
+++ unreachable_code.cpp
@@ -0,0 +1,25 @@
+#include <system_error>
+
+enum Enum
+{
+  Value1,
+  Value2,
+  Value3
+};
+
+int go(Enum t_enum) {
+  switch (t_enum) {
+  case Enum::Value1:               
+    return 0;
+  case Enum::Value2:               
+    return 1;
+  default:                         
+    throw std::runtime_error("unknown value");
+  }
+  throw std::runtime_error("unknown value"); // this code is unreachable
+}
+
+int main()
+{
+  go(Enum::Value3);
+}
